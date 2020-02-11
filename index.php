c7c72a35a98e3ee777710cbe154db80922b365b6
Kheih <56195049+Kheih@users.noreply.github.com>
2020-02-11 11:38:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
  $domOBJ = new DOMDocument();
- $domOBJ->load("https:// https://kheihxml.herokuapp.com/rss.php");//XML page URL
+ $domOBJ->load("https:// https://kheih.herokuapp.com/rss.php");//XML page URL
  $content = $domOBJ->getElementsByTagName("movie");
  
  ?>
