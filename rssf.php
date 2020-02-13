dc22750a920351e968eda9202e58039e5c550158
Kheih <56195049+Kheih@users.noreply.github.com>
2020-02-13 11:07:52 +0800

    Update rssf.php

--- rssf.php
+++ rssf.php
@@ -1,25 +1,25 @@
 <?php
-    $rss = '<?xml version="1.0" encoding="UTF-8"?>';
-    $rss .= '<rss version="2.0">';
-    $rss .= '<channel>';
+    $rssf = '<?xml version="1.0" encoding="UTF-8"?>';
+    $rssf .= '<rss version="2.0">';
+    $rssf .= '<channel>';
 
     $con = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820343") or die (mysqli_error($con));
     $sql = "SELECT * FROM tbl_movie";
     $q = mysqli_query($con, $sql) or die (mysqli_error($con));
-    
+
     while($r= mysqli_fetch_assoc($q)){
         extract($r);
         
-        $rss .= '<movie>';
-        $rss .= '<mov_title>' . $mov_title . '</mov_title>';
-        $rss .= '<mov_director>' . $mov_director . '</mov_director>';
-        $rss .= '<mov_writer>' . $mov_writer . '</mov_writer>';
-        $rss .= '<mov_artist>' . $mov_artist . '</mov_artist>';
-        $rss .= '<mov_genre>' . $mov_genre . '</mov_genre>';
-        $rss .= '</movie>';
+        $rssf .= '<movie>';
+        $rssf .= '<mov_title>' . $mov_title . '</mov_title>';
+        $rssf .= '<mov_director>' . $mov_director . '</mov_director>';
+        $rssf .= '<mov_artist>' . $mov_artist . '</mov_artist>';
+        $rssf .= '<mov_genre>' . $mov_genre . '</mov_genre>';
+        $rssf .= '<mov_writer>' . $mov_writer . '</mov_writer>';
+        $rssf .= '</movie>';
     }
-    $rss .= '</channel>';
-    $rss .= '</rss>';
+    $rssf .= '</channel>';
+    $rssf .= '</rss>';
 
-    echo $rss;
+    echo $rssf;
 ?>
