60546ba9cb6a8019a7568a6e46ca47f0f572a89e
Kheih <56195049+Kheih@users.noreply.github.com>
2020-02-13 11:10:07 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,26 @@
 <?php
  $domOBJ = new DOMDocument();
  $domOBJ->load("https://kheihxml.herokuapp.com/rssf.php");//XML page URL
-
+ 
  $content = $domOBJ->getElementsByTagName("movie");
 ?>
 
-<h1>Best Movies in 2019</h1>
+ <h1>Best Movie 2019</h1>
 
 <?php
- foreach($content as $data){
-     $title = $data->getElementsByTagName("mov_title")->item(0)->nodeValue;
-     $director = $data->getElementsByTagName("mov_director")->item(0)->nodeValue;
-     $writer = $data->getElementsByTagName("mov_writer")->item(0)->nodeValue;
-     $artist = $data->getElementsByTagName("mov_artist")->item(0)->nodeValue;
-     $genre = $data->getElementsByTagName("mov_genre")->item(0)->nodeValue;
-  
+ foreach( $content as $data ){
+     $mov_title = $data->getElementsByTagName("mov_title")->item(0)->nodeValue;
+     $mov_artist = $data->getElementsByTagName("mov_artist")->item(0)->nodeValue;
+     $mov_genre = $data->getElementsByTagName("mov_genre")->item(0)->nodeValue;
+     $mov_writer = $data->getElementsByTagName("mov_writer")->item(0)->nodeValue;
+     $mov_director = $data->getElementsByTagName("mov_director")->item(0)->nodeValue;
      echo "<ul>
-            <h2>$title</h2>
+            <h2>$mov_title</h2>
               <ul>
-                  <li>Director: $director </li>
-                  <li>Writer: $writer</li>
-                  <li>Artist: $artist </li>
-                  <li>Genre: $genre </li>
+                  <li>Director: $mov_director </li>
+                  <li>Artist: $mov_artist </li>
+                  <li>Genre: $mov_genre </li>
+                  <li>Writer: $mov_writer </li>
               </ul>
           </ul>";
  }
