14e2e8f8a7e039724ec5af3eb418950824238e99
Kheih <56195049+Kheih@users.noreply.github.com>
2020-10-03 22:12:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,285 @@
-<?php
- $domOBJ = new DOMDocument();
- $domOBJ->load("https://kheihxml.herokuapp.com/rss.php");//XML page URL
- 
- $content = $domOBJ->getElementsByTagName("movie");
-
-?>
-
- <h2> Best Movies in 2019 </h2>
-
-<?php
- foreach( $content as $data )
- {
-     $mov_title = $data->getElementsByTagName("mov_title")->item(0)->nodeValue;
-     $mov_director = $data->getElementsByTagName("mov_director")->item(0)->nodeValue;
-     $mov_writer = $data->getElementsByTagName("mov_writer")->item(0)->nodeValue;
-     $mov_artist = $data->getElementsByTagName("mov_artist")->item(0)->nodeValue;
-     $mov_genre = $data->getElementsByTagName("mov_genre")->item(0)->nodeValue;
-     
-  echo "<ul>
-            <h3>$mov_title</h3>
-              <ul>
-                  <li>Director: $mov_director </li>
-                  <li>Writer: $mov_writer </li>
-                  <li>Artist: $mov_artist </li>
-                  <li>Genre: $mov_genre </li>
-              </ul>
-          </ul>";
- }
-?>
+<!DOCTYPE html>
+<html>
+	<head> 
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+<header>
+		<div class="sky-h">
+			<div class="sun-h"></div>
+		<div id="background-wrap-h">
+		<div class="x1-h">
+			<div class="cloud-h"></div>
+		</div>
+		<div class="x2-h">
+			<div class="cloud-h"></div>
+		</div>
+		</div>
+		</div>
+		
+	<div class="grass-h">
+	  <div class ="panda-f">
+        <div class="face-f">
+          <div class ="ear-l"></div>
+          <div class ="ear-r"></div>
+          <div class ="eye-l">
+            <div class="eye-ball-f">
+            </div>
+          </div>
+          <div class ="eye-r">
+            <div class="eye-ball-f">
+            </div>
+          </div>
+          <div class="nose-f"></div>
+        </div>
+        <div class="hand-l"></div>
+        <div class="hand-r"></div>
+        <div class="leg-l"></div>
+        <div class="leg-r"></div>
+        <div class="shadow"></div>
+      </div>
+	  
+			<div class="body1-h">
+				<div class="face-d-h">
+					<div class="ear-left-h"></div>
+					<div class="ear-right-h"></div>
+				<div class="eye-right-h">
+					<div class="d1-h"></div>
+					<div class="d2-h"></div>
+				</div>
+				<div class="eye-left-h">
+					<div class="d3-h"></div>
+					<div class="d4-h"></div>
+				</div>
+					<div class="nose-d-h"></div>
+				</div>
+					<div class="hand-left-h"></div>
+					<div class="hand-right-h"></div>
+					<div class="leg-left-h"></div>
+					<div class="leg-right-h"></div>
+			</div>
+			
+        <div class="bamboo-h">
+			<div class="s1-h">
+				<div class="leaf1-h"></div>
+				<div class="leaf2-h"></div>
+				<div class="leaf3-h"></div>
+			</div>
+				<div class="s2-h"></div>
+				<div class="s3-h"></div>
+				<div class="s4-h"></div>
+		</div>
+	</div>
+   
+	<h2>panda & bamboo</h2>
+		<div class = "panda-logo-h">
+			<div class = "ear-h"></div>
+        <div class = "face-h">
+			<div class = "eye-shade-h"></div>
+				<div class = "eye-white-h">
+					<div class="shut-h">
+						<span></span>
+					</div>
+			<div class = "eye-ball-h"></div>
+				</div>
+			<div class = "eye-shade-h rgt"></div>
+				<div class = "eye-white-h rgt">
+					<div class="shut-h">
+						<span></span>
+					</div>
+			<div class = "eye-ball-h"></div>
+				</div>
+			<div class = " nose-h"></div>
+			<div class = " mouth-h"></div>
+		</div>
+</header>
+ <body>
+    <div class ="msg-container">
+	
+  <div class="bamboo joint2 bamboo1">
+    <div class="joint"></div>
+    <div class="joint"></div>
+  </div>
+  <div class="bamboo-group bamboo-group1">
+    <div class="bamboo">
+      <div class="joint">
+        <div class="leaf left left60 light"></div>
+        <div class="leaf left left30"></div>
+      </div>
+      <div class="joint"></div>
+      <div class="joint"></div>
+    </div>
+    <div class="bamboo">
+      <div class="joint">
+        <div class="leaf left left60 light"></div>
+        <div class="leaf left left30"></div>
+      </div>
+      <div class="joint"></div>
+    </div>
+    <div class="bamboo">
+      <div class="joint"></div>
+      <div class="joint">
+        <div class="leaf right right60 light"></div>
+        <div class="leaf right right90"></div>
+      </div>
+      <div class="joint"></div>
+    </div>
+  </div>
+  <div class="bamboo-group bamboo-group2">
+    <div class="bamboo">
+      <div class="joint">
+        <div class="leaf left left60 light"></div>
+        <div class="leaf left left30"></div>
+      </div>
+      <div class="joint"></div>
+      <div class="joint"></div>
+    </div>
+    <div class="bamboo">
+      <div class="joint">
+        <div class="leaf left left60 light"></div>
+        <div class="leaf left left30"></div>
+      </div>
+      <div class="joint"></div>
+    </div>
+    <div class="bamboo">
+      <div class="joint"></div>
+      <div class="joint">
+        <div class="leaf right right60 light"></div>
+        <div class="leaf right right90"></div>
+      </div>
+      <div class="joint"></div>
+    </div>
+  </div>
+  <div class="bamboo-group bamboo-group3 dark">
+    <div class="bamboo">
+      <div class="joint">
+        <div class="leaf left left60 light"></div>
+        <div class="leaf left left30"></div>
+      </div>
+      <div class="joint"></div>
+      <div class="joint"></div>
+    </div>
+    <div class="bamboo">
+      <div class="joint">
+        <div class="leaf left left60 light"></div>
+        <div class="leaf left left30"></div>
+      </div>
+      <div class="joint"></div>
+    </div>
+    <div class="bamboo">
+      <div class="joint"></div>
+      <div class="joint">
+        <div class="leaf right right60 light"></div>
+        <div class="leaf right right90"></div>
+      </div>
+      <div class="joint"></div>
+    </div>
+  </div>
+  <div class="bamboo-group bamboo-group4 dark">
+    <div class="bamboo">
+      <div class="joint">
+        <div class="leaf left left60 light"></div>
+        <div class="leaf left left30"></div>
+      </div>
+      <div class="joint"></div>
+      <div class="joint"></div>
+    </div>
+    <div class="bamboo">
+      <div class="joint"><div class="leaf left left60 light"></div><div class="leaf left left30"></div></div>
+      <div class="joint"></div>
+    </div>
+    <div class="bamboo">
+      <div class="joint"></div>
+      <div class="joint"><div class="leaf right right60 light"></div><div class="leaf right right90"></div></div>
+      <div class="joint"></div>
+    </div>
+    <div class="bamboo">
+      <div class="joint"><div class="leaf left left60 light"></div><div class="leaf left left30"></div></div>
+      <div class="joint"></div>
+    </div>
+  </div>
+		<div class="msg-col">
+			<h1> Welcome to Panda & Bamboo</h1>
+			<p>"A panda's daily diet consists almost entirely of the leaves, stems and shoots of various bamboo species. Learn why these much-loved animals are endangered in their bamboo forest homes. Discover the panda's surprising skill at swimming and climbing."</p>
+			<a href="">Get Started</a>
+		</div>
+	</div>
+   
+   <div class="wrapper">
+  <div class="btn1">
+    <button type="button">Sign-up</button>
+    </div>
+</div>
+		<div class = "panda">
+			<div class = "ear"></div>
+		<div class = "face">
+			<div class = "eye-shade"></div>
+        <div class = "eye-white">
+            <div class="shut">
+				<span></span>
+			</div>
+			<div class = "eye-ball"></div>
+        </div>
+			<div class = "eye-shade rgt"></div>
+        <div class = "eye-white rgt">
+			<div class="shut">
+				<span></span>
+			</div>
+			<div class = "eye-ball"></div>
+        </div>
+			<div class = " nose"></div>
+			<div class = " mouth"></div>
+        </div>
+			<div class = "body"></div>
+        <div class = "foot">
+            <div class = "finger"></div>
+        </div>
+        <div class = "foot rgt">
+			<div class = "finger"></div>
+        </div>
+		</div>
+		
+		<form>
+			<div class = "hand"></div>
+			<div class = "hand rgt"></div>
+        
+        <h1>Sign-up Panda</h1>
+			<div class ="form-group">
+				<input required="required" class="form-control"/>
+				<label class="form-label">Username</label>
+			</div>
+        <div class ="form-group">
+			<input type="password" required="required" class="form-control"/>
+				<label class="form-label">Password</label>
+			<div><button class= "btn2">Login</button></div>
+        </div>
+		</form>
+    </body>
+	<footer>
+			<ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+  
+	  <div class="area" >
+ <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel ="stylesheet">
+   
+  <div class = "social-menu">
+    <ul>
+      <li><a href = "#"><i class="fa fa-facebook"></i></a> </li>
+       <li><a href = "#"><i class="fa fa-twitter"></i></a> </li>
+       <li><a href = "#"><i class="fa fa-instagram"></i></a> </li>
+       <li><a href = "#"><i class="fa fa-linkedin"></i></a> </li>
+       <li><a href = "#"><i class="fa fa-pinterest"></i></a> </li>
+    </ul>
+  </div>
+	  </div >
+    </footer>
+</html>
